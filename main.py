8b30acca429237cdf39813e8fae4b7a0da64110a
lineman60 <snyder60>
2021-05-16 23:07:56 -0500

    next commit

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 # importing required modules
 import PyPDF2
+import glob
+
+def read_pdf(pdf_file):
+    pdfFileObj = open(pdf_file, 'rb')
+    # creating a pdf reader object
+    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+    total_pages = int(pdfReader.numPages)
+    # creating a page object
+    pageObj = pdfReader.getPage(0)
+    for pagenumb in range(0,total_pages ):
+        # extracting text from page
+        print(pageObj.extractText())
+    pdfFileObj.close()
 
 # creating a pdf file object
-pdfFileObj = open('Be_Ye_Transformed.pdf', 'rb')
+file_to_read = glob.glob('*.pdf')
+for x in file_to_read:
+    read_pdf(x)
 
-# creating a pdf reader object
-pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-
-# printing number of pages in pdf file
-print(pdfReader.numPages)
-
-# creating a page object
-pageObj = pdfReader.getPage(0)
-
-# extracting text from page
-print(pageObj.extractText())
-
-# closing the pdf file object
-pdfFileObj.close()
